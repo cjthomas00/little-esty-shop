2c288f89fd9b8c6df56047b47058f39223797964
KaraJH <karas.jones@yahoo.com>
2023-02-24 15:31:49 -0700

    feat: Disabled and enabled merchants in index and model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,12 @@ class Merchant < ApplicationRecord
   has_many :items
 
   enum status: ["Enabled", "Disabled"]
+
+  def self.enabled_merchants 
+    where(status: 0)
+  end
+
+  def self.disabled_merchants 
+    where(status: 1)
+  end
 end
