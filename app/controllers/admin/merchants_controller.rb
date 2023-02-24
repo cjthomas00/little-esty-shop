a4c49933a2f8898f2f0ce5a14a63a5cedbf9b231
KaraJH <karas.jones@yahoo.com>
2023-02-23 19:24:42 -0700

    feat: Update form with flash message

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,4 +6,20 @@ class Admin::MerchantsController < ApplicationController
   def show 
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit 
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update 
+    merchant = Merchant.find(params[:id])
+    merchant.update!(update_merchant_attributes)
+    redirect_to "/admin/merchants/#{merchant.id}"
+    flash[:notice] = "Successfully Updated"
+  end
+
+  private 
+  def update_merchant_attributes
+    params.require(:merchant).permit(:name)
+  end
 end
