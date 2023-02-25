cb0954dc1ca0a0e041b9290c43b0b35cf5b55309
Caleb Thomas <cjthomas00@comcast.net>
2023-02-24 20:32:43 -0700

    Adds user story 4 Created ::items_ready_to_ship method in Item.rb model. Created spec tests for model, and features for user story 4. Created items_ready_to_ship section in merchant dashboard in item controller and views. Co-authored-by: JTaylor28

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
+    @items_ready_to_ship = Item.ready_to_ship(params[:merchant_id])
   end
 end
