e0e329051e5ed2606e72e749e91df6d4ebc0128d
Elle Majors <hellolovely@ellemajors.com>
2023-02-26 15:50:58 -0700

    add: test and view for user story 34

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe 'admin invoice show page' do
   before :each do 
     @invoice = FactoryBot.create_list(:invoice, 3)
+    @item = FactoryBot.create_list(:item, 5)
+    @invoice_item = FactoryBot.create_list(:invoice_item, 3, invoice: @invoice[1], item: @item[1])
+    # require 'pry'; binding.pry
   end
 
   describe 'user story 33' do
@@ -19,28 +22,19 @@ describe 'admin invoice show page' do
       end  
     end
   end
-#   34. Admin Invoice Show Page: Invoice Item Information
 
-# As an admin
-# When I visit an admin invoice show page
-# Then I see all of the items on the invoice including:
-# - Item name
-# - The quantity of the item ordered
-# - The price the Item sold for
-# - The Invoice Item status
+  describe 'user story 34' do
+    it 'displays all of the items on the invoice including the name, quantity, price, and status' do
+      # visit admin_invoices_path(@invoice[1].id)
+      visit "/admin/invoices/#{@invoice[1].id}"
+      # save_and_open_page
+      within(".invoices_item_info") do
 
-describe 'user story 34' do
-  it 'displays all of the items on the invoice including the name, quantity, price, and status' do
-    # visit admin_invoices_path(@invoice[1].id)
-    visit "/admin/invoices/#{@item[1].id}"
-
-    within(".invoices_item_info") do
-
-      expect(page).to have_content(@item[1].name)
-      expect(page).to have_content(@item[1].quantity)
-      expect(page).to have_content(@item[1].price)
-      expect(page).to have_content(@item[1].status)
-    end  
+        expect(page).to have_content(@invoice[1].invoice_items[1].item.name)
+        expect(page).to have_content(@invoice[1].invoice_items[1].item.quantity)
+        expect(page).to have_content(@invoice[1].invoice_items[1].item.unit_price)
+        expect(page).to have_content(@invoice[1].invoice_items[1].item.status.capitalize)
+      end  
+    end
   end
-end
 end    
