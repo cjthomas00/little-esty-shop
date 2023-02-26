9d72ca19065c996cfef8bf5fd676b254f4b0c482
Caleb Thomas <cjthomas00@comcast.net>
2023-02-25 20:45:43 -0700

    create test for first part of US11 item creation.

--- spec/features/merchants/items/create_spec.rb
+++ spec/features/merchants/items/create_spec.rb
@@ -7,10 +7,16 @@ RSpec.describe 'Merchant Items new' do
       @item1 = @merchant1.items.create!(name: "Yeti bottle", description: "24oz metal container for water", unit_price: 48, status: 1) 
       @item2 = @merchant1.items.create!(name: "Football", description: "toy for kids", unit_price: 45, status: 1)
     end
+
     describe "I see a link to create a new item. when I click on the link," do
       it "I am taken to a form that allows me to add item information." do
-        visit merchant_dashboard_index_path(@merchant1.id)
+        visit "/merchants/#{@merchant1.id}/items"
+        
         click_link("New Item")
+        expect(page).to have_content("Create New Item")
+        expect(page).to have_field("Item Name:")
+        expect(page).to have_field("Description:")
+        expect(page).to have_field("Price:")
       end
     end
   end
