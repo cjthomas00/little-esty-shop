42264d80c844d960a2d95955f56d00e02d67e53c
James Taylor <jrtaylor0614@gmail.com>
2023-02-25 22:37:12 -0700

    Adds merchant invoice invoice_items to show page

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe 'Merchant Invoices Show Page' do
     @item1 = @merchant1.items.create!(name: "Yeti bottle", description: "24oz metal container for water", unit_price: 48) 
     @customer1 = Customer.create!( first_name: "Britney", last_name: "Johnson")
     @invoice1 = @customer1.invoices.create!( status: 1, created_at: 'Wednesday, February 22, 2023') 
-    InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 11, unit_price: @item1.unit_price, status: 1)
-
+    @invoice_item = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 11, unit_price: @item1.unit_price, status: 1)
+    @merchant2 = 
   end
   # User Story 15
   describe "As a merchant," do
@@ -21,7 +21,22 @@ RSpec.describe 'Merchant Invoices Show Page' do
         expect(page).to have_content("Created on: #{@invoice1.created_at.strftime("%A, %B %d, %Y")}")
         expect(page).to have_content("Customer:")
         expect(page).to have_content("#{@customer1.first_name + " " + @customer1.last_name}")
-      end 
+      end
+       # User Story 16
+      describe "Then I see all of my items on the invoice including:" do
+        it "The items name, quantity ordered, price sold for, and Invoice Item status" do
+          visit merchant_invoice_path(@merchant1.id, @invoice1.id) 
+
+          expect(page).to have_content("Items on this Invoice :")
+          within "#item-" do 
+            save_and_open_page
+            expect(page).to have_content("#{@invoice1.items.first.name}")
+            expect(page).to have_content("Units: #{@invoice_item.quantity}")
+            expect(page).to have_content("Price: $48.00")
+            expect(page).to have_content("Status: #{@invoice1.invoice_items.first.status}")
+          end 
+        end
+      end
     end 
   end 
 end 
