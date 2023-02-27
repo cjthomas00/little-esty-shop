a4943c0df1aef13899c9301b19effcd0579377f1
KaraJH <karas.jones@yahoo.com>
2023-02-27 13:43:13 -0700

    feat: Update invoice status buttons

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -6,4 +6,17 @@ class Admin::InvoicesController < ApplicationController
   def show
    @invoice = Invoice.find(params[:id])
   end
+
+  def update 
+    # require 'pry'; binding.pry
+    invoice = Invoice.find(params[:id])
+    invoice.update!(update_status)
+    redirect_to "/admin/invoices/#{invoice.id}"
+  end
+
+  private 
+  
+  def update_status
+    params.require(:invoice).permit(:status)
+  end
 end
