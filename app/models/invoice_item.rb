179a17ab54fd4335ccdc013193a6f2b26aeff55c
Caleb Thomas <cjthomas00@comcast.net>
2023-02-27 21:24:27 -0700

    add validations and #top_sales_date.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -5,5 +5,17 @@ class InvoiceItem < ApplicationRecord
   has_one :merchant, through: :item
   has_one :customer, through: :invoice
 
+  validates_presence_of :quantity, :unit_price
+  validates :quantity, :unit_price, numericality: {greater_than_or_equal_to: 0}
   enum status: ["pending", "packaged", "shipped"]
+
+  def self.top_sales_date
+    select('invoices.*, sum(invoice_items.quantity * invoice_items.unit_price) as total_revenue')
+    .joins(:transactions)
+    .where(transactions: {result: :success})
+    .group('invoices.id')
+    .order('total_revenue desc, invoices.created_at desc')
+    .first
+    .created_at
+  end
 end
